a2efe308d66f5b3c04f33982eff987d28805839b
pshometver <pshometver@gmail.com>
2026-09-01 16:31:16 +0300

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -343,7 +343,7 @@ document.addEventListener('DOMContentLoaded', function() {
         currentIndex: 0,
         totalSlides: 0,
         interval: null,
-        autoplayDelay: 5000, // 5 секунд
+        autoplayDelay: 7500, // 7.5 секунд
         isTransitioning: false,
         
         init() {
